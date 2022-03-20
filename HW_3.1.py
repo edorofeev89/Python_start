8ad26e5e3525cd0f7997f6813103be7b876146bb
EDorofeev <85448490+edorofeev89@users.noreply.github.com>
2022-03-20 12:37:47 +0200

    'HW3'

--- HW_3.1.py
+++ HW_3.1.py
@@ -1,6 +1,8 @@
 input_parameter = 'можно добавить что-то в'
+
 result_1 = f'Это строка в которую {input_parameter} новую строку'
 result_2 = result_1.replace(input_parameter, 'замена в строке')
+
 print(input_parameter)
 print(result_1)
 print(result_2)
