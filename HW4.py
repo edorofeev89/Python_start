1ac60dc6fc0846f93ae7943601a5ff01f6d51b26
EDorofeev <85448490+edorofeev89@users.noreply.github.com>
2022-03-21 20:38:19 +0200

    HW 4 v2

--- HW4.py
+++ HW4.py
@@ -1,6 +1,10 @@
-input_parameter = input('Введите данные:')
+input_parameter = int(input('Введите число:'))
+result = 0
 
-if input_parameter.isdigit():
-    print(input_parameter[::-1])
-else:
-    print('Введите число')
+while input_parameter != 0:
+    numb = input_parameter % 10
+    input_parameter = input_parameter // 10
+
+    result = result * 10 + numb
+
+print(result)
